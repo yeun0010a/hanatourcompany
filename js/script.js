1a303691c19324c4cc3b3eab1aad520af6b0d9c9
yeun0010a <151604961+yeun0010a@users.noreply.github.com>
2024-01-23 14:32:57 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(function(){
 // 메뉴
-$(".pcBigMenu, .subMenu>li").mouseenter(function(){
+$(".pcBigMenu, .subMenu>li, subMenuBG").mouseenter(function(){
     $('.subMenu>li').stop().slideDown(200);
     $('.subMenuBG').stop().slideDown(200);
 });
-$(".pcBigMenu, .subMenu>li").mouseleave(function(){
+$(".pcBigMenu, .subMenu>li, subMenuBG").mouseleave(function(){
     $('.subMenu>li').stop().slideUp(200);
     $('.subMenuBG').stop().slideUp(200);
 });
