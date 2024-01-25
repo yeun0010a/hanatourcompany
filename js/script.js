aca7854f44bb8ae6654c456e9e5da5561df4dc74
yeun0010a <151604961+yeun0010a@users.noreply.github.com>
2024-01-25 13:31:27 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(function(){
 $("#menu").slicknav();
 // pc 슬라이드 업다운
 $("#menu>li,#menu>li>ul, #smenu_bar").mouseover(function(){
-    $("#menu>li>ul, #smenu_bar").stop().slideDown("200");  
+    $("#menu>li>ul, #smenu_bar").stop().slideDown("fast");  
 });
 $(".nav, #smenu_bar").mouseleave(function(){
-    $("#menu>li>ul, #smenu_bar").stop().slideUp("100");  
+    $("#menu>li>ul, #smenu_bar").stop().slideUp("fast");  
 });
 
 
